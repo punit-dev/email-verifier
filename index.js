3d28533ae7c4e7cf2a59d0c697039c65ef52176d
punit-dev <punitpoddar2310@gmail.com>
2026-03-16 11:58:44 +0530

    add express server for hosted email verification API

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
+// const verifyEmail = require("./src/verifyEmail");
+
+// const run = async (email) => {
+//   const result = await verifyEmail(email);
+//   console.log(result);
+// };
+
+// const email = process.argv[2];
+
+// run(email);
+
+require("@dotenvx/dotenvx").config();
+const express = require("express");
 const verifyEmail = require("./src/verifyEmail");
 
-const run = async (email) => {
-  const result = await verifyEmail(email);
-  console.log(result);
-};
+const app = express();
 
-const email = process.argv[2];
+app.get("/verify", async (req, res) => {
+  const email = req.query.email;
 
-run(email);
+  if (!email) {
+    return res.status(400).json({ error: "Email query parameter required." });
+  }
+
+  try {
+    const result = await verifyEmail(email);
+    res.json({ message: "Email verification result.", result });
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT: ${PORT}`);
+});
